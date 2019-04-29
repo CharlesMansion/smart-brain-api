db98ea2af8f52b9dc48bce83f6a7ef5622ea74b1
Nicolas Nollomont <nicolasnollomont@iMac-de-Nicolas.lan>
2019-04-29 22:32:05 +0200

    correcting bug with tables

--- postgres/tables/login.sql
+++ postgres/tables/login.sql
@@ -3,7 +3,9 @@ BEGIN TRANSACTION;
 CREATE TABLE login (
     id serial PRIMARY KEY,
     hash VARCHAR(100) NOT NULL,
-    email text UNIQUE NOT NULL
+    email text UNIQUE NOT NULL,
+    age SMALLINT,
+    pet VARCHAR(100)
 );
 
 COMMIT;
